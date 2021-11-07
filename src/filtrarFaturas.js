4b64f3d2a15c53a0c392d1ae69936acba458921e
Ana-Marie <anamarie1907@gmail.com>
2021-11-07 19:39:21 -0300

    ciclo 1- 3. refatorando

--- src/filtrarFaturas.js
+++ src/filtrarFaturas.js
@@ -1,6 +1,6 @@
 function filtrar(arrFaturas) {
-    const newArrFaturas = arrFaturas.filter(fatura => fatura.valor >= 2000);
-    return newArrFaturas;
+    return arrFaturas.filter(fatura => fatura.valor >= 2000);
+
 }
 
 module.exports = filtrar;
