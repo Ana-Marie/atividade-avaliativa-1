ef8b1a7430fa05cc8dedceadd0cbac0c3e1ce247
Ana-Marie <anamarie1907@gmail.com>
2021-11-07 19:33:16 -0300

    ciclo 1 -2. teste passando

--- src/filtrarFaturas.js
+++ src/filtrarFaturas.js
@@ -1,5 +1,6 @@
-function filtrar(arrFaturas){
-    
+function filtrar(arrFaturas) {
+    const newArrFaturas = arrFaturas.filter(fatura => fatura.valor >= 2000);
+    return newArrFaturas;
 }
 
 module.exports = filtrar;
